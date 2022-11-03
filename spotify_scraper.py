58ced4014e3f40ca0a438257c59e76950101ad87
jong-inn <bequiet13@gmail.com>
2022-11-02 23:18:46 -0500

    complete scraper function in spotify_scraper.py

--- spotify_scraper.py
+++ spotify_scraper.py
@@ -1,5 +1,6 @@
 
 import os
+import argparse
 import json
 import time
 from spotify_api import SpotifyAPI
@@ -19,37 +20,33 @@ if len(file_list) == 0:
 
 class SpotifyScraper:
     
+    # initialize the instance
     def __init__(self):
-        
         pass
-    
-    def albums_scraper(self):
+            
+    def scraper(self, type_, all=False):
+        
+        response_key = type_.replace("-", "_")
         
         spotify = SpotifyAPI()
         spotify.get_token()
         
-        pass
-    
-    def artists_scraper(self):
-        
-        spotify = SpotifyAPI()
-        spotify.get_token()
-        
-        pass
-    
-    def tracks_scraper(self):
-        
-        spotify = SpotifyAPI()
-        spotify.get_token()
-        
-        pass
-    
-    def audio_features_scraper(self):
-        
-        spotify = SpotifyAPI()
-        spotify.get_token()
-        
-        pass
+        for matched_file_path, type_extracted, target_keys in self.extract_targets(type_, all):
+            start_time = time.time()
+            print(f"{matched_file_path}")
+            
+            try:
+                for batch_keys in self.make_batches(type_, target_keys):
+                    response = spotify.get_query_by_ids(type_, batch_keys)
+                    type_extracted.update(dict(zip(batch_keys, response[response_key])))
+            except Exception as e:
+                print(e)
+            finally:
+                with open(matched_file_path, "w") as f:
+                    f.write(json.dumps(type_extracted))
+                
+                end_time = time.time()
+                print(f"elapsed time: {round((end_time - start_time) / 60, 2)} mins")
     
     def extract_targets(self, type_, all=False):
         
@@ -62,18 +59,21 @@ class SpotifyScraper:
             
             # load type_.extracted.mpd.slice file
             if all:
-                type_extracted = None
+                type_extracted = {}
             else:
                 matched_file_path = os.sep.join((path, type_, f"{type_}.{file}"))
                 if os.path.exists(matched_file_path):
                     with open(matched_file_path, "r") as f:
                         type_extracted = json.loads(f.read())
                 else:
-                    type_extracted = None
+                    type_extracted = {}
 
             # find target keys
             target_keys = self.extract_different_keys(type_, extracted, type_extracted)
-            yield matched_file_path, type_extracted, target_keys
+            if len(target_keys) == 0:
+                continue
+            else:
+                yield matched_file_path, type_extracted, target_keys
     
     def extract_different_keys(self, type_, extracted, type_extracted):
         if type_ == "albums":
@@ -92,35 +92,43 @@ class SpotifyScraper:
         else:
             return list(keys_extracted)
 
-            
-if __name__ == "__main__":
-    
-    # spotify = SpotifyAPI()
-    # spotify.get_token()
-    
-    # with open(os.sep.join((path, file_list[0])), "r") as f:
-    #     tmp = json.loads(f.read())
-    #     tmp_keys = list(tmp.keys())
+    def make_batches(self, type_, target_keys):
+        if type_ == "albums":
+            batch_size = 20
+        elif type_ == "artists":
+            batch_size = 50
+        elif type_ == "tracks":
+            batch_size = 50
+        elif type_ == "audio-features":
+            batch_size = 100
         
-    # tmp_dict = {}
+        for idx in range(0, len(target_keys), batch_size):
+            if idx % 20000 == 0:
+                print(f"{type_} scraping progress: {round(idx / len(target_keys) * 100, 2)}")
+            
+            yield target_keys[idx:idx + batch_size]
 
-    # try:
-    #     for idx in range(0, len(tmp), 100):
-            
-    #         if idx % 1000 == 0:
-    #             print(f"progress: {round((idx / len(tmp)) * 100, 2)} %")
-            
-    #         r = spotify.get_query_by_ids("audio-features", tmp_keys[idx:idx+100])
-    #         tmp_dict.update(dict(zip(tmp_keys[idx:idx+100], r["audio_features"])))
-    # except Exception as e:
-    #     print(e)
-    # finally:
-    #     with open(f"./extracted_data/{'audio-features.' + file_list[0]}", "w") as f:
-    #         f.write(json.dumps(tmp_dict))
     
-    # for test
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="scraping spotify data using its api")
+    
+    parser.add_argument("-t", "--type"
+                        , action="store"
+                        , type=str
+                        , choices=["albums", "artists", "tracks", "audio-features"]
+                        , help="one of albums, artists, tracks, audio-features"
+                        , required=True
+                        )
+    
+    parser.add_argument("-a", "--all"
+                        , action="store"
+                        , type=bool
+                        , choices=[True, False]
+                        , default=False
+                        , help="if it is true, scrape all"
+                        , required=False
+                        )
+    args = parser.parse_args()
+    
     scraper = SpotifyScraper()
-    for matched_file_path, type_extracted, target_keys  in scraper.extract_targets("audio-features"):
-        print(f"matched_file_path: {matched_file_path}")
-        print(f"type_extracted   : {True if type_extracted else False}")
-        print(f"target_keys      : {len(target_keys)}")
+    scraper.scraper(type_ = args.type, all = args.all)
