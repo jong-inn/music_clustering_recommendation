1b2defc9afa43ab2d2ce8e72ff45ca54baa84ff9
jong-inn <bequiet13@gmail.com>
2022-12-07 23:45:42 -0600

    make eda and preprocess directory and move some python scripts to these folders and add a sampled_data/sample_tracks_audio_features.py file

---
+++ sampled_data/sample_tracks_audio_features.py
@@ -0,0 +1,17 @@
+# packages & path variables
+
+import os
+import pandas as pd
+from preprocess.spotify_tracks_preprocess import pre_processed_df as p1
+from preprocess.spotify_audio_features_preprocess import pre_processed_df as p2
+
+pickle_path = "sampled_data_200000.pickle"
+
+sample_ids = pd.read_pickle(pickle_path)
+print(sample_ids.head())
+
+tracks = p1()
+print(tracks.head())
+
+audio_features = p2()
+print(audio_features.head())
