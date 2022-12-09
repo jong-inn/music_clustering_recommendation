cbae2143db99b4a1bc209ed256993b4bcc7440aa
jong-inn <bequiet13@gmail.com>
2022-12-08 23:47:42 -0600

    update setup.sql

--- mysql/setup.sql
+++ mysql/setup.sql
@@ -1,7 +1,8 @@
 -- CREATE DATABASE spotify;
 
-DROP TABLE AudioFeatures;
+-- DROP TABLE AudioFeatures;
 CREATE TABLE AudioFeatures (
+    track_id Varchar(24),
 	acousticness Float,
     danceability Float,
     duration_ms Int,
@@ -13,5 +14,32 @@ CREATE TABLE AudioFeatures (
     speechiness Float,
     tempo Float,
     time_signature Int,
-    valence Float
+    valence Float,
+    PRIMARY KEY (track_id)
 );
+
+-- DROP TABLE Tracks;
+CREATE TABLE Tracks (
+    track_id Varchar(24),
+    album_id Varchar(24),
+    artists_id Varchar(24),
+    disc_number Int,
+    duration_ms Int,
+    explicit Boolean,
+    name Varchar(80),
+    popularity Int,
+    track_number Int,
+    PRIMARY KEY (track_id)
+);
+
+-- DROP TABLE Artists;
+CREATE TABLE Artists (
+    artist_id Varchar(24),
+    PRIMARY KEY (artist_id)
+);
+
+-- DROP TABLE Albums;
+CREATE TABLE Albums (
+    album_id Varchar(24),
+    PRIMARY KEY (album_id)
+)
