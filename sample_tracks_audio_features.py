f8b97f0f57cf22406842c8a2bb985714414ad55a
jong-inn <bequiet13@gmail.com>
2022-12-09 23:46:44 -0600

    update sample_tracks_audio_features.py

--- sample_tracks_audio_features.py
+++ sample_tracks_audio_features.py
@@ -5,10 +5,11 @@ import pandas as pd
 from preprocess.spotify_tracks_preprocess import pre_processed_df as p1
 from preprocess.spotify_audio_features_preprocess import pre_processed_df as p2
 
-pickle_path = "sampled_data_200000.pickle"
+pickle_path = "./sampled_data/sampled_data_200000.pickle"
 
 sample_ids = pd.read_pickle(pickle_path)
 print(sample_ids.head())
+print(sample_ids.track_id.nunique())
 
 tracks = p1()
 print(tracks.head())
