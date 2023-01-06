3328a729f1326c20e5161d7360cef2792d25dfb5
jong-inn <bequiet13@gmail.com>
2023-01-05 22:44:06 -0600

    update setup.sql

--- mysql/setup.sql
+++ mysql/setup.sql
@@ -1,40 +1,61 @@
 
-
+-- CREATE DATABASE spotify;
 USE spotify;
 
+CREATE TABLE artist (
+	artist_id Varchar(22) NOT NULL,
+    name Varchar(300),
+    genres Varchar(300),
+    popularity Int,
+    PRIMARY KEY (artist_id)
+);
 
-CREATE TABLE temp_total (
-	track_id Varchar(22),
-    album_id Varchar(22),
-    disc_number Int,
+CREATE TABLE album (
+	album_id Varchar(22) NOT NULL,
+    popularity Int,
+    release_date Varchar(20),
+    PRIMARY KEY (album_id)
+);
+
+CREATE TABLE track (
+	track_id Varchar(22) NOT NULL,
+    album_id Varchar(22) NOT NULL,
+    name Varchar(300),
     duration_ms Int,
-    explicit Bool,
-    track_name Varchar(300),
-    album_popularity Int,
-    track_number Int,
-    duration_m Float,
-    release_date Varchar(10),
-    release_year Int,
-    release_year_bin Int,
+    popularity Int,
+    PRIMARY KEY (track_id),
+    FOREIGN KEY (album_id) REFERENCES album (album_id)
+);
+
+CREATE TABLE audio_features (
+	pseudo_id Int AUTO_INCREMENT,
+    tempo Float,
+    valence Float,
+    danceablitiy Float,
     acousticness Float,
-    danceability Float,
     energy Float,
     instrumentalness Float,
     liveness Float,
     loudness Float,
-    mode Int,
     speechiness Float,
-    tempo Float,
-    time_signature Int,
-    valence Float,
-    artist_popularity Int,
-    genres_combined Varchar(1800),
-	artist_name_combined Varchar(300),
-    artist_popularity_max Int
+    track_id Varchar(22),
+    PRIMARY KEY (pseudo_id, track_id),
+    FOREIGN KEY (track_id) REFERENCES track (track_id)
+						   ON DELETE CASCADE
 );
 
-CREATE TABLE temp_track_artist (
-	track_id Varchar(22),
-    artist_id Varchar(22),
-    artist_name Varchar(100)
+CREATE TABLE artist_album (
+	artist_id Varchar(22),
+    album_id Varchar(22),
+    PRIMARY KEY (artist_id, album_id),
+    FOREIGN KEY (artist_id) REFERENCES artist (artist_id),
+    FOREIGN KEY (album_id) REFERENCES album (album_id)
+);
+
+CREATE TABLE artist_track (
+	artist_id Varchar(22),
+    track_id Varchar(22),
+    PRIMARY KEY (artist_id, track_id),
+    FOREIGN KEY (artist_id) REFERENCES artist (artist_id),
+    FOREIGN KEY (track_id) REFERENCES track (track_id)
 );
